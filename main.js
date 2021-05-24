517e676671cf23af7664897fa2ab7069696bab4c
sth49 <ghdehdgml12345@naver.com>
2021-05-25 00:00:17 +0900

    resize

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require("path");
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
-    height: 600,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
